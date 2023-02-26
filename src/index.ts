7a6a1e4c44d3be1b65bb3dfa559c3974fd5334e8
Rich McCluskey <rich@dozuki.com>
2023-02-26 00:12:12 -0800

    Install discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello there");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.login(process.env.CLIENT_TOKEN);
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  console.log(message.content);
+});
