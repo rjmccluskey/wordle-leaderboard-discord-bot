1d3a3591a48289762a71262da6befaf3903af0b3
Rich McCluskey <rich@dozuki.com>
2023-03-11 13:12:29 -0800

    Make discord client shareable

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Events, GatewayIntentBits } from "discord.js";
 import cron from "node-cron";
 import { handleSlashCommands } from "./commands";
 import { onNewMessage } from "./onNewMessage";
 import { saveAndAnnounceLatestScores } from "./saveAndAnnounceLatestScores";
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+import { client } from "./client";
 
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
@@ -19,7 +12,7 @@ client.once(Events.ClientReady, (c) => {
 client.on(Events.MessageCreate, onNewMessage);
 client.on(Events.InteractionCreate, handleSlashCommands);
 
-client.login(process.env.CLIENT_TOKEN);
+client.login(process.env.CLIENT_TOKEN).catch((error) => console.error(error));
 
 // Run every day at 12:05am
 // The extra 5 minutes hopefully helps if the bot is first added right before midnight and is still backfilling.
