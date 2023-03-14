830da555e95c83081a3c8bb4c76b9144a2cf3f91
Rich McCluskey <rich@dozuki.com>
2023-03-13 23:26:45 -0700

    Use croner instead of node-cron
    There's a bad memory leak in node-cron
    https://github.com/node-cron/node-cron/issues/358

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Events, GatewayIntentBits } from "discord.js";
-import cron from "node-cron";
+import { Events } from "discord.js";
+import { Cron } from "croner";
 import { handleSlashCommands } from "./commands";
 import { onNewMessage } from "./onNewMessage";
 import { saveAndAnnounceLatestScores } from "./saveAndAnnounceLatestScores";
@@ -16,7 +16,8 @@ client.login(process.env.CLIENT_TOKEN).catch((error) => console.error(error));
 
 // Run every day at 12:05am
 // The extra 5 minutes hopefully helps if the bot is first added right before midnight and is still backfilling.
-cron.schedule("5 0 * * *", saveAndAnnounceLatestScores, {
-  timezone: "America/Los_Angeles",
-  scheduled: process.env.NODE_ENV === "production",
-});
+Cron(
+  "5 0 * * *",
+  { timezone: "America/Los_Angeles" },
+  saveAndAnnounceLatestScores
+);
